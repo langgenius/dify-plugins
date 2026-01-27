48618bab0169777aee1e721a6538c7ae11ab8746
Rasmus <uusipaikka.rasmus@gmail.com>
2026-01-26 22:40:15 -0800

    Remove source files, keep only .difypkg package

--- thetokencompany/ttc_compression/main.py
+++
@@ -1,8 +0,0 @@
-from dify_plugin import Plugin
-from dify_plugin.config.config import DifyPluginEnv
-
-config = DifyPluginEnv()
-plugin = Plugin(config)
-
-if __name__ == "__main__":
-    plugin.run()
