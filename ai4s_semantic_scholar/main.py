f90f732002463544fd19f0809d323fa08f8264ef
literaf <zhongrugu@gmail.com>
2026-01-28 16:01:21 +0800

    chore: Remove source directory, keep only package file

--- ai4s_semantic_scholar/main.py
+++
@@ -1,9 +0,0 @@
-from dify_plugin import Plugin
-from dify_plugin.config.config import DifyPluginEnv
-
-# 从 .env 文件自动加载配置
-config = DifyPluginEnv()
-plugin = Plugin(config)
-
-if __name__ == "__main__":
-    plugin.run()
