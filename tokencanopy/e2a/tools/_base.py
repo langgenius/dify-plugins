4b22a6807daf19d3e6a256d7cf90d9a20a43913a
Josh Zhang <39790535+jiashuoz@users.noreply.github.com>
2026-08-08 17:21:38 -0700

    chore(e2a): submit packaged plugin only

--- tokencanopy/e2a/tools/_base.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from typing import Any
-
-from tools._client import E2AClient
-
-
-def e2a_client(tool: Any) -> E2AClient:
-    return E2AClient(tool.runtime.credentials.get("e2a_api_key", ""))
